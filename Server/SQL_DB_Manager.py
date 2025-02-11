b28ddf81cb54dd05b2d3f2b528694d503192334b
OrTrabelsi <gamma-user65@greendreamteam.onmicrosoft.com>
2025-02-12 01:56:39 +0200

    added functions signatures and fixed some functions

--- Server/SQL_DB_Manager.py
+++ Server/SQL_DB_Manager.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 
 TABLES = {'Ingredients_Table': "CREATE TABLE `Ingredients_Table` ("
     "  `id` int(11) NOT NULL AUTO_INCREMENT,"
-    "  `Name` varchar(255) NOT NULL,"
+    "  `Name` varchar(15) NOT NULL,"
     "  `Estimated Expiry` int(11) NOT NULL,"
     "  `Unit Size` varchar(255) NOT NULL,"
     ") ENGINE=InnoDB",
@@ -16,18 +16,24 @@ TABLES = {'Ingredients_Table': "CREATE TABLE `Ingredients_Table` ("
 }
 
 config = {"host": "localhost", "user": "admin", "password": "admin"}
+DB_NAME = "HungryDB"
 
 
 
 class SQL_DB_Manager:
-
-
-
     def __init__(self):
-
         self.DB = mysql.connector.connect(**config)
         self.DBcursor = self.DB.cursor()
-
+        try:
+            self.DBcursor.execute("USE {}".format(DB_NAME))
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("DB doesnt exist")
+                createDB()
+                self.DB.database = DB_NAME
+            else:
+                print(err.msg) #switch to log
+        
 
     def __del__(self):
         self.DBcursor.close()
@@ -35,6 +41,7 @@ class SQL_DB_Manager:
 
     def createDB(self):
         self.DBcursor.execute("CREATE DATABASE HungryDB")
+        #call create tables functions
 
     
 
@@ -44,10 +51,7 @@ class SQL_DB_Manager:
             #print("Creating table {}: ".format(table_name), end='') to log
             self.DBcursor.execute(table_description)
         except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                print("already exists.") #to log
-            else:
-                print(err.msg) #to log
+            print(err.msg) #to log
 
     def create_inventories_table(self):
         table_description = TABLES['Inventories_Table']
@@ -55,29 +59,38 @@ class SQL_DB_Manager:
             #print("Creating table {}: ".format(table_name), end='') to log
             self.DBcursor.execute(table_description)
         except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                print("already exists.") #to log
-            else:
-                print(err.msg) #to log
-    
+            print(err.msg) #to log
+
+
+    def create_Tables(self): #optional
+        for table in TABLES:
+            table_description = TABLES['Inventories_Table']
+            try:
+                #print("Creating table {}: ".format(table_name), end='') to log
+                self.DBcursor.execute(table_description)
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                    print("already exists")
+                else:
+                    print(err.msg) #to log
+
+
+
+    def get_ingerdients_by_ids(self, ids ):
+        pass
+
+    def get_password(self, inv_id):
+        pass
+    def get_inventory_items(self, inv_id):
+        pass
+    def update_inventory_recipes(self, inv_id, recipe):
+        pass
+    def get_custom_recipes(self, inv_id):
+        pass
+
+
 
     
-        
 
-    
-
-    def response(self):
-        return self.request_dic["response"]
-
-
-
-
-dbCursor = 
-
-def ConnectDb()
-
-def CreateDB(DBcursor):
-
-    DBcursor.execute("Create DATA")
 
 
