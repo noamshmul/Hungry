c4b72606bc3ff713cf9a369048eccd41d4aa6777
Savva <gamma-user63@greendreamteam.onmicrosoft.com>
2025-02-14 09:33:48 +0200

    feat: add basic fastapi

--- Server/gateway.py
+++ Server/gateway.py
@@ -1,33 +1,17 @@
-import socket
+from fastapi import FastAPI
 import logging
 
 logger = logging.getLogger(__name__)
 
-SERVER_HOST = "127.0.0.1"
-SERVER_PORT = 7777
+app = FastAPI()
 
-SERVER_MAX_PARALLEL_CONNECTIONS = 5
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-def bind_and_listen():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((SERVER_HOST, SERVER_PORT))
-    s.listen(SERVER_MAX_PARALLEL_CONNECTIONS)
-    logger.info(f"Listening on {SERVER_HOST}:{SERVER_PORT}")
-
-    try:
-        while True:
-            conn, addr = s.accept()
-    
-            logger.info(f'Connected to {addr[0]}:{addr[1]}')
-    
-            # Start a new thread and return its identifier
-
-    except KeyboardInterrupt as e:
-        logger.info("Got an keyboard interrupt- closing the server")
-    except Exception as e:
-        logger.critical("Closing server with exception:", e)
-    s.close()
-
+@app.get("/users")
+def read_root(id : int | None):
+    return {"Hello": "World"}
 
 if __name__ == '__main__':
     import log
