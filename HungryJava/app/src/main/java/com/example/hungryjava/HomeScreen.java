1f443a2f5e6435e9608488860cc28bbf5b969d21
TamirHoch28 <tamirmechina@gmail.com>
2025-02-16 14:14:00 +0200

    basic homescreen

--- HungryJava/app/src/main/java/com/example/hungryjava/HomeScreen.java
+++ HungryJava/app/src/main/java/com/example/hungryjava/HomeScreen.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,14 +21,24 @@ public class HomeScreen extends AppCompatActivity {
         TextView wellcome = findViewById(R.id.welcomeView);
         wellcome.setText("Hello " + username);
 
+        // logout button
+        Button logout = findViewById(R.id.logoutButton);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code to execute when the button is clicked}
+                Toast.makeText(HomeScreen.this, "Logout clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // fridge button
         Button fridge = findViewById(R.id.fridgeButton);
         fridge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code to execute when the button is clicked}
-                return;
-            };
+                Toast.makeText(HomeScreen.this, "fridge clicked", Toast.LENGTH_SHORT).show();
+            }
         });
 
         // I'm Hungry button
@@ -36,8 +47,8 @@ public class HomeScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Code to execute when the button is clicked}
-                return;
-            };
+                Toast.makeText(HomeScreen.this, "i'm Hungry clicked", Toast.LENGTH_SHORT).show();
+            }
         });
 
 
