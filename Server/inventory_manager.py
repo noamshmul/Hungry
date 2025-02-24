62b3b15ac19282b938bf4c8717e89f8cdba1d4bf
Savva <gamma-user63@greendreamteam.onmicrosoft.com>
2025-02-24 14:27:44 +0200

    feat: add Inventory_manager outline

---
+++ Server/inventory_manager.py
@@ -0,0 +1,20 @@
+
+
+class Inventory_Manager:
+    def get_password(inventory_id : int):
+        raise NotImplementedError
+
+    def get_inventory(inventory_id : int):
+        raise NotImplementedError
+    
+    def add_item():
+        raise NotImplementedError
+
+    def remove_item():
+        raise NotImplementedError
+    
+    def get_all_custom_recipes():
+        raise NotImplementedError
+    
+    def add_custom_recipe():
+        raise NotImplementedError
