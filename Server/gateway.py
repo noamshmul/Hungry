ad132d8aff0b1150c989fe6684cdb6b55c64f86d
Savva <gamma-user63@greendreamteam.onmicrosoft.com>
2025-02-25 17:14:06 +0200

    feat: add inventory get post delete

--- Server/gateway.py
+++ Server/gateway.py
@@ -11,17 +11,18 @@ def test_connection(inventory_id = Depends(authentication)):
     '''Used to test if server is alive and if auth is valid'''
     return {"Hello": "World", "inventory_id" :inventory_id}
 
-# @router.get("/users")
-# def read_users(id : int = 4):
-#     return {"Hello": "World"}
+@router.get("/inventory")
+def get_inventory(inventory_id = Depends(authentication)):
+    items = inventory_manager.get_inventory(inventory_id)
+
+    return {"items" : items}
 
 @router.post("/inventory")
-def add_item(name : str, amount : int):
-    inventory_manager.add_item(name, amount)
+def add_item(name : str, amount : int, inventory_id = Depends(authentication)):
+    inventory_manager.add_item(name, amount, inventory_id)
     return {"status": "ok"}
 
-@router.get("/inventory")
-def get_inventory():
-    # TODO: 
-
-    return {"status": "ok", "items" : items}
+@router.delete("/inventory")
+def remove_item(name : str, amount : int, inventory_id = Depends(authentication)):
+    inventory_manager.remove_item(name, amount, inventory_id)
+    return {"status": "ok"}
