84c7afcc1e7aac6656da5d30ffcc28d7d3462beb
EladLaviMagen <gamma-user05@greendreamteam.onmicrosoft.com>
2025-02-28 13:02:42 +0200

    New branch

--- HungryJava/app/src/main/java/com/example/hungryjava/api/FastApiService.java
+++ HungryJava/app/src/main/java/com/example/hungryjava/api/FastApiService.java
@@ -1,8 +1,16 @@
 package com.example.hungryjava.api;
 import java.util.Map;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 public interface FastApiService {
     @GET("/users")
     Call<Map<String, Object>> getInventory();
+
+    @GET("/users")
+    Call<Map<String, Object>> addItem();
+
+    @DELETE("/inventory")
+    Call<Map<String, Object>> removeItem();
+
 }
