b8dc88c7dbca90c2ad7132e46565fd73bc6a2089
TamirHoch28 <tamirmechina@gmail.com>
2025-03-04 15:25:39 +0200

    login and signup

--- Server/gateway.py
+++ Server/gateway.py
@@ -3,19 +3,20 @@ from fastapi import Depends, APIRouter
 from auth import authentication
 import inventory_manager
 from log import logger
+import logging
 
 router = APIRouter()
 
 @router.get("/test-connection")
 def test_connection(inventory_id = Depends(authentication)):
     '''Used to test if server is alive and if auth is valid'''
-    return {"Hello": "World", "inventory_id" :inventory_id}
+    return {"Hello": "World", "inventory_id": inventory_id}
 
 @router.get("/inventory")
-def get_inventory(inventory_id = Depends(authentication)):
+def get_inventory(inventory_id=Depends(authentication)):
+    logger.info(f"Iventory id: {inventory_id}")
     items = inventory_manager.get_inventory(inventory_id)
-
-    return {"items" : items}
+    return {"items": items}
 
 @router.post("/inventory")
 def add_item(name : str, amount : int, inventory_id = Depends(authentication)):
@@ -32,7 +33,13 @@ def get_custom_recipes(inventory_id = Depends(authentication)):
     custom_recipes = inventory_manager.get_all_custom_recipes(inventory_id)
     return custom_recipes
 
+
 @router.post("/custom-recipes")
 def add_custom_recipes(name : str, instructions : list, approx_time : int, ingredients : list, size : int, inventory_id = Depends(authentication)):
     inventory_manager.add_custom_recipe(name, instructions, approx_time, ingredients, size,inventory_id)
+    return {"status": "ok"}
+
+
+@router.post("/signup")
+def signup(inventory_id: str, password: str):
     return {"status": "ok"}
