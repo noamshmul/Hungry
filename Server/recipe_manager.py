76c1a7be158bc1771f56bfe2eef21694a2ae37d7
Savva <gamma-user63@greendreamteam.onmicrosoft.com>
2025-03-10 19:57:39 +0200

    feat[server]: add recipes mock

--- Server/recipe_manager.py
+++ Server/recipe_manager.py
@@ -5,4 +5,7 @@ func = Recipes.MongoDB_Functions(base)
         
 def hungry():
     # the big algorithm
-    pass
+    
+    #placeholder
+    recipes = [func.get_recipe_by_id("54a40a326529d92b2c003c00"), func.get_recipe_by_id("54a40a1f19925f464b37396a") , func.get_recipe_by_id("54a40a1c19925f464b373961")]
+    return recipes
