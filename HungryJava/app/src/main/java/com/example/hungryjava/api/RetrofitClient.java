16b52311a3923fef95d23bdb0debccda8ea806f2
Savva <gamma-user63@greendreamteam.onmicrosoft.com>
2025-03-26 11:37:13 +0200

    feat: add PROD/DEV base url

--- HungryJava/app/src/main/java/com/example/hungryjava/api/RetrofitClient.java
+++ HungryJava/app/src/main/java/com/example/hungryjava/api/RetrofitClient.java
@@ -4,6 +4,8 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Interceptor;
 import okhttp3.Response;
+
+import android.os.Build;
 import android.util.Base64;
 import java.io.IOException;
 import retrofit2.Retrofit;
@@ -16,6 +18,7 @@ import android.util.Log;
 
 public class RetrofitClient {
     private static final String BASE_URL = "http://10.0.2.2:8000";
+    private static final String PROD_URL = "http://192.168.211.128:8000";
     private static Retrofit retrofit = null;
     public static Retrofit getRetrofitInstance(String username, String password, boolean flag) {
         if (retrofit == null || flag) {
@@ -38,13 +41,31 @@ public class RetrofitClient {
                     })
                     .build();
 
+            String base_url;
+            if (isAndroidEmulator()) {
+                base_url = BASE_URL;
+            }
+            else { base_url = PROD_URL; }
+
+
             // create the Retrofit instance with OkHttpClient
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(base_url)
                     .client(client)  //attach the custom OkHttpClient
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return retrofit;
+    }
+
+    private static boolean isAndroidEmulator() {
+        String model = Build.MODEL;
+        String product = Build.PRODUCT;
+        boolean isEmulator = false;
+        if (product != null) {
+            isEmulator = product.equals("sdk") || product.contains("_sdk") || product.contains("sdk_");
+        }
+        Log.d("Retrofit", "isEmulator=" + isEmulator);
+        return isEmulator;
     }
 }
